06042a7a7566a0ced5564cad5d0d5628f3d52290
Dafari Al Fathri <106307220+dafarialfathri@users.noreply.github.com>
2024-04-09 17:33:00 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+const inkomen = document.getElementById('komen');
+var fotonya = document.getElementById('isifoto');
+var target = ['winda sari putri', 'fanny fatti rahmah', 'annisa mutya', 'penti eprilia'];
+const pesan = {
+    "winda sari putri": "slamat idul fitri ya win, aku minta maaf buat semua kesalahan aku ke kamu. Gw liat keknya lu udah lancar ngegambar lagi ya? kalo gitu gw turut seneng berarti kondisi lu udah pulih total ya? semoga aja udah. oiya sebelumnya maaf juga pernah maksa lu buat bales perasaan gw, gw sadar kalo perasaan emang gabisa dipaksa. Soal lu di rumah, lu happy-happy aja apa gimana? apa lu udah boleh keluar dari rumah?. Sama tolong titip salam buat ibu sama ayah lu ya win, bilangin maaf dari gw buat semua-semuanya. Gw paham kebahagiaan mereka ada di lu, jadi karena terakhir gw bikin lu kenapa-kenapa jadi kebahagiaan mereka jadi ilang, gw minta maaf soal itu juga. Soal kebahagiaan lu, semoga lu tiap hari makin happy ya win, semoga ga sedih lagi kek sesedih lu dikatain sama dosen soal lu yang belum kerja itu, sama pas lu lagi sedih pas kehilangan pen tab lu itu, sama yg pasti pas lu lagi sedih karena kondisi abis kecelakaan itu. Semoga lu ga sesedih itu lagi, karena janji gw ke diri gw sendiri dari awal usahain biar lu ga kena dua hal, 1. sedih, 2. sakit. gw cuma bisa doa dari sini setiap kali inget lu, karena gw gabisa lagi komunikasi sama lu karena lu nya udah gamau lagi, itu dari yg gw pahamin. oiya masih inget foto itu ga win? wkwk itu momen pertama kali gw seneng banget bisa ngabulin keinginan lu pergi ke pantai. udah itu aja, intinya selamat lebaran ya win :D",
+    "fanny fatti rahmah": "selamat idul fitri ya fan, maafin gw kalo ada salah. gw tau emang soal perasaan gabisa dipaksa, makanya gw terakhir ngomong kalo emang waktu Deket ga juga lu bisa ninggalin doi, gw pamit, karena gw simpulin klo bukan gw yang lu mau. gw pamit karena lu gapantes sama gw yg mudah dibodohin sama cewe. gw pernah setinggi itu kemauan gw hidup bareng sama lu, tapi kalo lu nya aja ga berkenan, yaudah gw bisa apa. Semoga pilihan lu bisa bikin lu bahagia ya, gw cuma bisa doa dari sini setiap kali inget lu. oiya lu inget ga foto ini? foto pas gw sebenernya ke medan bela-belain mau ketemu keluarga lu. sebenernya gw mau liatin foto ganci yang gw kasih ke lu apa masih lu simpen apa ngga gw gatau wkwk. Udah itu aja dari gw, selamat lebaran fan.",
+    "annisa mutya": "hai vel sayang, selamat lebaran ya, mohon maaf lahir batin. klo kamu baca ini berarti kamu udah termasuk yang spesial buat aku. terimakasih ya sayang udah pulihin lagi perasaanku setelah life after break up yang bikin aku hampir mati rasa, akhirnya aku punya rasa lagi sama sosok perempuan kayak kamu. aku bilang aku mau serius sama kamu, punya status sama kamu, tapi itu yang belum aku liat dari kamu apakah kamu serius sama yang lagi kita jalanin ini. Aku gatau ujungnya kita bakal gimana, tapi aku udah terimakasih banget udah dibangkitin dari life after break up yang hampa itu. Selebihnya itu keputusan kamu mau gimana sama aku, aku gatau yg jelas apapun yang akan kamu lakuin dan apa yg kamu rasa itu cuma kamu dan Tuhan yang tau. udah itu aja sedikit curahan hati aku. selamat lebaran sayang ❤️",
+    "penti eprilia": "hai lia, apa kabar? btw selamat lebaran ya, aku minta maaf sama semua kesalahan aku ke kamu. Aku minta maaf sama semua yang bikin kamu ga nyaman sama aku, ya mungkin salah satunya karena aku minta hubungan yang lebih  sama kamu, tapi aku sadar kalo bukan aku orang yang kamu mau, kamu cuma kagum aja sama aku dan engga lebih. walaupun begitu aku beneran soal perasaan ke kamu lia, aku sayang sama kamu. Makanya aku iyain semua permintaan kamu, termasuk ga chatan lagi. Sama siapapun kamu, aku cuma bisa berdoa disini biar kamu selalu happy dan ga sedih-sedihan lagi. Semangat ya buat selesaiin kuliah teknik yang susah itu wkwk. Sekali lagi, selamat lebaran lia :)",
+    "melsi oktaviana": "hai mel, selamat lebaran ya, mohon maaf lahir batin. Mel mungkin secara dak langsung lah banci jo dafa yo kan. Dafa ndak tau, kini lah kacau bana setelah mencari-cari yang dak pasti. banyak hubungan yang dak cocok dafa jalani setelah mel itu, makonyo pas dafa sempat balik ke mel kapatang tuh, dafa ragu karena dafa takut jadi mangacauan mel. oiyo mel baa kaba nyo? intinyo selamat lebaran yo mel, dafa minta maaf untuk sado kekurangan dan kesalahan dafa samo mel. dafa masih nyimpan perasaan samo mel tapi baa lah mungkin mel dak berkenan lai",
+    "nadia refiyanah": "hai mbak nadd, selamat lebaran yee, gw minta maaf buat semua kesalahan gw. Jujur gw selalu kebayang dari rasa bersalah ngasih lu harapan, tapi itu emang bener nyatanya dari gw, gw beneran sayang sama lu, tapi gw takut nanti kalo di waktu gw yang masih gatau nasibnya ini, gw takut masih blom bisa mulai buat jalan hubungan. intinya gitu deh, mungkin lu udah tutup hati buat gw, gw terima konsekuensinya. btw semoga hapy lebarannya yee nad, gw sayang lu"
+}
+const images = {
+    "winda sari putri": "winda sari putri.jpg",
+    "fanny fatti rahmah": "fanny fatti rahmah.jpg",
+    "annisa mutya": "annisa mutya.jpg",
+    "penti eprilia": "yall.jpg",
+    "melsi oktaviana": "yall.jpg",
+    "nadia refiyanah": "yall.jpg",
+    "default": "yall.jpg"
+};
+const nameInput = document.getElementById("komen");
+const profileImage = document.getElementById("isifoto");
+
+nameInput.addEventListener("input", function() {
+    const name = nameInput.value.trim();
+    if (name === "") {
+        messageContainer.innerHTML = "";
+        profileImage.src = "";
+    }
+})
+
+function tampilpesan() {
+    const messageContainer = document.getElementById("messageContainer");
+
+    const name = nameInput.value.trim();
+    if (name === "") {
+        alert("Masukkan nama lengkap kamu");
+        return;
+    }
+
+    const message = pesan[name] || "Halo " + name + ", assalamualaikum! mohon maaf lahir & batin :)";
+    const messageElement = document.createElement("p");
+    messageElement.textContent = message;
+
+    messageContainer.innerHTML = "";
+    messageContainer.appendChild(messageElement);
+
+    const imageSrc = images[name] || images["default"];
+    profileImage.src = imageSrc;
+}
